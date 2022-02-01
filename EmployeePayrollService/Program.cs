91ec3740798948c47fb6b3bb45547b7ae0580bd5
ak47 <kokitkarak@gmail.com>
2022-02-01 19:18:02 +0530

    retrieve data from database and add new data to the database

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -11,23 +11,23 @@ namespace EmployeePayrollService
             EmployeeRepo repo = new EmployeeRepo();
             repo.ConnectionString();
 
-            EmployeePayroll employee = new EmployeePayroll();
+            EmployeePayroll employeePayroll = new EmployeePayroll();
             //employee.id = 5;
-            employee.name = "Akshay";
-            employee.basic_pay = 10000.00M;
-            employee.StartDate = Convert.ToDateTime("2021-11-06");
-            employee.Gender = "M";
-            employee.phone = "9868511791";
-            employee.address = "pune";
-            employee.Department = "Mechanical";
-            employee.deductions = 1500.00;
-            employee.taxable_pay = 1500.00;
-            employee.income_tax = 1000.00;
-            employee.net_pay = 1400.00;
+            employeePayroll.name = "Akshay";
+            employeePayroll.basic_pay = 10000.00M;
+            employeePayroll.StartDate = Convert.ToDateTime("2021-11-06");
+            employeePayroll.Gender = "M";
+            employeePayroll.phone = "9868511791";
+            employeePayroll.address = "pune";
+            employeePayroll.Department = "Mechanical";
+            employeePayroll.deductions = 1500.00;
+            employeePayroll.taxable_pay = 1500.00;
+            employeePayroll.income_tax = 1000.00;
+            employeePayroll.net_pay = 1400.00;
             
-            if (repo.AddEmployee(employee))
+            if (repo.AddEmployee(employeePayroll))
                 Console.WriteLine("Records added successfully");
-            repo.AddEmployee(employee);
+            repo.AddEmployee(employeePayroll);
         }
     }
 }
